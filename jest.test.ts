9e1e6f4098005077341f1a6824097901272aa5a6
Sperlin <sperling.gabriel@gmail.com>
2022-04-21 17:21:21 +0200

    edit package.json for test report

--- jest.test.ts
+++ jest.test.ts
@@ -1,7 +1,9 @@
-test('test jenkins pipeline2', () => {
-    expect(2+2).toBe(4);
-})
-
-test('test jenkins pipeline3', () => {
-    expect(2+2).toBe(4);
+describe("jenkins pipeline tests", () => {
+    test('test jenkins pipeline', () => {
+        expect(2+2).toBe(4);
+    })
+    
+    test('test jenkins pipeline2', () => {
+        expect(2+2).toBe(4);
+    })
 })

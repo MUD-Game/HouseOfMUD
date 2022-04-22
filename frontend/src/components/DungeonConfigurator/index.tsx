3a0b9b889fbc5d338ace1ad8d2819811633c9c44
raphaelsack <i20029@hb.dhbw-stuttgart.de>
2022-04-22 18:41:31 +0200

    added location in DungeonConfigurator

--- frontend/src/components/DungeonConfigurator/index.tsx
+++ frontend/src/components/DungeonConfigurator/index.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
+import { Container } from 'react-bootstrap';
+import { useLocation } from 'react-router-dom';
 
 export interface DungeonConfiguratorProps { }
 
+interface LocationState {
+    action: "new" | "edit";
+}
+
 const DungeonConfigurator: React.FC<DungeonConfiguratorProps> = ({ }) => {
+    const location = useLocation();
+    let action = (location.state as LocationState)?.action || "new";
 
     return (
-        <div>
+
+        <Container>
             <h1>DungeonConfigurator</h1>
-        </div>
+        </Container>
+
     )
 }
 

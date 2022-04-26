0b63c368e52e8bb68aea8c14fa72b4adad81fcce
Fabian <fabian.klimpel2002@gmail.com>
2022-04-26 21:22:15 +0200

    pain
    Co-authored-by: niklasar <niklasar@users.noreply.github.com>

--- frontend/src/components/Game/Chat/ChatOutput.tsx
+++ frontend/src/components/Game/Chat/ChatOutput.tsx
@@ -11,22 +11,53 @@ import React, { useState } from 'react'
 import { useRabbitMQ } from 'src/hooks/useRabbitMQ';
 import { useEffect } from 'react';
 import { Row } from 'react-bootstrap';
+
 export interface ChatOutputProps {}
 
-const ChatOutput: React.FC<ChatOutputProps> = ({ }) => {
 
-    const [messages, setMessages] = useState("Mock");
+const ChatOutput: React.FC<ChatOutputProps> = () => {
+
+    console.log('ASDasdasdasd');
+    
+    const [messages, setMessages] = useState<string[]>([]);
+
+    // const [message, setMessage] = useState<string>();
+
+    // const [messages, setMessages] = useState("Mock");
+
+    // const messages: string[] = [];
+
     const {setChatSubscriber} = useRabbitMQ();
 
-    setChatSubscriber((message:string)=>{
-        setMessages(message)
+    let addMessage = (message: string) => {
+        setMessages([...messages, message]);
+    };
+
+    setChatSubscriber((data: any)=>{
+
+        addMessage(data.message);
+
+        // messages.push(data.message);
+        // setMessage(data.message);
+
+        // setMessages([...messages!, data.message]);
+        // setMessage({ message: message });
+        // setMessages(message)
     });
     
     return (
         <Row>
             <div className="col">
                 <span>CHAT-OUTPUT</span>
-                <div className="mock-placeholder chat drawn-border p-2">{messages}</div>      
+                <div className="mock-placeholder chat drawn-border p-2">
+                    {messages.map((message, index) => {
+                        return (
+                            <div key={index} className={"chat-message channel-global"}>
+                                {message}
+                            </div>
+                        )
+                    })}
+                </div>      
             </div>
         </Row>
     )

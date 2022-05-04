7be086011cff6bd9f20c8e15d0b5eb6d68a57254
niklasar <niklas.arnold@web.de>
2022-05-04 18:43:30 +0200

    ChatQueue implemented

--- frontend/src/components/DungeonMaster/ChatQueue.tsx
+++ frontend/src/components/DungeonMaster/ChatQueue.tsx
@@ -6,14 +6,30 @@
  */
 
 import React from 'react'
+import { Row } from 'react-bootstrap';
+import { Send } from 'react-bootstrap-icons';
 export interface ChatQueueProps {
-    commands: any;
+    commandQueue: string[];
+    onSendQueue:VoidFunction;
 }
 
-const ChatQueue: React.FC<ChatQueueProps> = ({ commands }) => {
+const ChatQueue: React.FC<ChatQueueProps> = ({ commandQueue, onSendQueue }) => {
     return (
-        <div className="chatqueue drawn-border mb-2">
-            <p className='text-center pt-5'>Chat-Queue</p>
+        <div className="chatqueue drawn-border mb-2 p-2 ps-0">
+            <div className="chatqueue-wrap h-100 pe-1">
+                <ul className='ps-4'>
+                    {commandQueue.map((command, index) =>
+                        <li key={index}>{ command }</li>
+                    )}
+                </ul>
+                { commandQueue !== [] ?
+                <div className="p-2">
+                    <button className='btn drawn-border btn-green w-100' onClick={ onSendQueue }>
+                        <Send />
+                    </button>
+                </div>
+                : "" }
+            </div>
         </div>
     )
 }

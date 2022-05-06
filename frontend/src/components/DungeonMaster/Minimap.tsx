13c02f66e11899132617303b7dba825259b9961a
raphaelsack <i20029@hb.dhbw-stuttgart.de>
2022-05-06 21:53:18 +0200

    fullscreen minimap dungeonMaster

--- frontend/src/components/DungeonMaster/Minimap.tsx
+++ frontend/src/components/DungeonMaster/Minimap.tsx
@@ -4,14 +4,13 @@
  * @description Minimap Component to display the currently visible Minimap
  * @props {@linkcode MinimapProps}
  */
-import React, { useRef } from 'react'
+import React, { useRef, useLayoutEffect } from 'react'
 import { Circle, Group, Layer, Line, Rect, Stage, Text } from 'react-konva';
 import { MudRoom } from 'src/types/dungeon';
 import { useRabbitMQ } from '../../hooks/useRabbitMQ';
 import { useEffect } from 'react';
 import './index.css'
-import $ from 'jquery'
-import { ArrowsFullscreen, Compass, Fullscreen, GeoAlt } from 'react-bootstrap-icons';
+import { ArrowsAngleContract, ArrowsFullscreen, Compass, Fullscreen, FullscreenExit, GeoAlt } from 'react-bootstrap-icons';
 import compassPng from 'src/assets/compass.png';
 import Konva from 'konva';
 import { useRefSize } from '../../hooks/useRefSize';
@@ -21,7 +20,8 @@ const roomMargin = 40
 const roomOffset = roomSize + roomMargin;
 const connectionStrokeWidth = 10;
 const connectionBlobRadius = connectionStrokeWidth;
-const initialScale = 0.8;
+const initialScaleSmall = 0.8;
+const initialScaleLarge = 4;
 const minScale = 0.1;
 const maxScale = 10;
 
@@ -75,12 +75,29 @@ const Minimap: React.FC<MinimapProps> = (props) => {
     const currentPositionCircleRef = useRef<any>();
     const [currentRoomId, setCurrentRoomId] = React.useState<string>(props.startRoom);
     const [rooms, setRooms] = React.useState<MiniMapData["rooms"]>(props.rooms);
-    const [width, height] = useRefSize(sizeRef);
-
-
-    useEffect(() => {
-        focusOnRoom("0,0", true);
+    // const [width, height] = useRefSize(sizeRef);
+    const [isFullscreen, setIsFullscreen] = React.useState<boolean>(false);
+    const [size, setSize] = React.useState<{ width: number, height: number }>({ width: 0, height: 0 });
+    
+    // Get the new Size of sizeRef on resize
+    useLayoutEffect(() => {
+        let windowListener = () => setSize({ width: sizeRef?.current?.clientWidth, height: sizeRef?.current?.clientHeight });
+        window.addEventListener('resize', windowListener);
+        windowListener();
+        return () => {
+            window.removeEventListener('resize', windowListener);
+        }
     }, []);
+
+    useLayoutEffect(() => {
+        focusOnRoom("0,0", isFullscreen);
+    }, [isFullscreen]);
+
+    // useEffect(() => {
+    //     console.log("d")
+    //     const s = sizeRef.current.getBoundingClientRect();
+    //     setSize({ width: s.width, height: s.height });
+    // }, [sizeRef]);
 
     const toggleConnection = (event: any) => {
         // Change the attrs of the connection
@@ -133,42 +150,31 @@ const Minimap: React.FC<MinimapProps> = (props) => {
     }
 
 
-    const focusOnRoom = (roomId: string, isAnc: boolean) => {
-        if (isAnc && stageRef.current) {
+    const focusOnRoom = (roomId: string, isFullscreen: boolean) => {
+        if (stageRef.current) {
             const xc = parseInt((roomId || currentRoomId).split(",")[0]);
             const yc = parseInt((roomId || currentRoomId).split(",")[1]);
-            const x = -xc * roomOffset * initialScale;
-            const y = -yc * roomOffset * initialScale;
-            stageRef.current.to({ x: x, y: y, duration: 0.4, scaleX: initialScale, scaleY: initialScale, easing: Konva.Easings.EaseInOut });
+            const usedScale = isFullscreen ? initialScaleLarge : initialScaleSmall;
+            const x = -xc * roomOffset * usedScale;
+            const y = -yc * roomOffset * usedScale;
+            stageRef.current.to({ x: x, y: y, duration: 0.4, scaleX: usedScale, scaleY: usedScale, easing: Konva.Easings.EaseInOut });
         }
     }
 
     return (
-        <>
-            <div id="konva-buttons-container">
-                <img src={compassPng} alt="compass" id="compass" onClick={() => {
-                    focusOnRoom(currentRoomId, true);
+        <div ref={sizeRef}>
+            <div id={`konva-buttons-container${isFullscreen ? "-fullscreen" : ""}`}>
+                <img src={compassPng} draggable={false} alt="compass" id={`compass${isFullscreen ? "-fullscreen" : ""}`} onClick={() => {
+                    focusOnRoom(currentRoomId, isFullscreen);
                 }} />
-                <ArrowsFullscreen size={40} onClick={() => {
-                    $("body").toggleClass("noclickevents");
-                    $('#minimap').toggleClass('fullscreen');
-                    // Check if the stage is already fullscreen
-                    if ($('#minimap').hasClass('fullscreen')) {
-                        stageRef.current.setAttrs({
-                            width: window.innerWidth,
-                            height: window.innerHeight,
-                        })
-                    } else {
-                        stageRef.current.to({
-                            width: width,
-                            height: width,
-                            duration: 0.01,
-                        })
-                    }
-                }} />
+                {isFullscreen ? <ArrowsAngleContract id={`fullscreenbutton-fullscreen`} size={40} onClick={() => {
+                    setIsFullscreen(!isFullscreen);
+                }} /> : <ArrowsFullscreen id={`fullscreenbutton`} size={40} onClick={() => {
+                    setIsFullscreen(!isFullscreen);
+                }} />}
             </div>
-            <div id="minimap" className="mb-2" ref={sizeRef}>
-                <Stage ref={stageRef} onWheel={onWheelHandle} width={width} height={width} draggable offsetY={(-width / 2) / initialScale + (roomSize / 2)} offsetX={(-width / 2) / initialScale + (roomSize / 2)}>
+            <div id={`minimap${isFullscreen ? "-fullscreen" : ""}`} className="mb-2">
+                {!isFullscreen ? <Stage ref={stageRef} onWheel={onWheelHandle} width={size.width} height={size.width} draggable offsetY={(-size.width / 2) / initialScaleSmall + (roomSize / 2)} offsetX={(-size.width / 2) / initialScaleSmall + (roomSize / 2)}>
                     <Layer>
                         <Group name="rooms">
                             {Object.keys(rooms).map((roomkey, index) => {
@@ -231,7 +237,7 @@ const Minimap: React.FC<MinimapProps> = (props) => {
                                     verticalAlign: "middle"
                                 }
                                 let text = new Konva.Text(textConfig)
-                                console.log(text.getSize());
+                                // console.log(text.getSize());
 
                                 return (
                                     <Group key={roomkey}>
@@ -258,9 +264,99 @@ const Minimap: React.FC<MinimapProps> = (props) => {
                         </Group>
                     </Layer>
                 </Stage>
+                    : <Stage onWheel={onWheelHandle} width={window.innerWidth} scaleX={initialScaleLarge} scaleY={initialScaleLarge} height={window.innerHeight} draggable offsetY={(-window.innerHeight / 2) / (initialScaleLarge) + (roomSize / 2)} offsetX={-window.innerWidth / 2 / (initialScaleLarge) + (roomSize / 2)}>
+                    <Layer>
+                        <Group name="rooms">
+                            {Object.keys(rooms).map((roomkey, index) => {
+                                const room = rooms[roomkey];
+                                let fillColor = fillActive;
+                                let strokeColor = strokeActive;
+                                const x = room.xCoordinate;
+                                const y = room.yCoordinate;
+                                let xStrokeCol = connectionOpen;
+                                let yStrokeCol = connectionOpen;
+                                switch (room.connections.east) {
+                                    case 'open':
+                                        xStrokeCol = connectionOpen;
+                                        break;
+                                    case 'inactive':
+                                        xStrokeCol = connectionInactive;
+                                        break;
+                                    case 'closed':
+                                        xStrokeCol = connectionClosed;
+                                        break;
+                                }
+                                switch (room.connections.south) {
+                                    case 'open':
+                                        yStrokeCol = connectionOpen;
+                                        break;
+                                    case 'inactive':
+                                        yStrokeCol = connectionInactive;
+                                        break;
+                                    case 'closed':
+                                        yStrokeCol = connectionClosed;
+                                        break;
+                                }
+                                const xStatus = rooms[roomkey].connections.east;
+                                const yStatus = rooms[roomkey].connections.south;
+
+
+                                const xPoints = [x * roomOffset + roomSize + 2, y * roomOffset + (roomSize / 2), (x + 1) * roomOffset, y * roomOffset + (roomSize / 2)];
+
+                                const yPoints = [x * roomOffset + (roomSize / 2), y * roomOffset + roomSize + 2, x * roomOffset + (roomSize / 2), (y + 1) * roomOffset];
+
+                                let connections: React.ReactNode[] = [];
+
+
+                                (xStatus !== 'inactive') && connections.push(<Line key={`${roomkey}-connection-east`} points={xPoints} stroke={xStrokeCol} data-room={roomkey} strokeWidth={connectionStrokeWidth} data-status={xStatus} onClick={toggleConnection} data-direction={'east'} />);
+
+
+                                (yStatus !== 'inactive') && connections.push(<Line key={`${roomkey}-connection-south`} points={yPoints} stroke={yStrokeCol} data-room={roomkey} strokeWidth={connectionStrokeWidth} data-status={yStatus} onClick={toggleConnection} data-direction={'south'} />);
+
+                                const textConfig = {
+                                    x: x * roomOffset,
+                                    y: y * roomOffset,
+                                    align: "center",
+                                    width: roomSize,
+                                    height: roomSize,
+                                    fontSize: 10,
+                                    fontFamily: 'Segoe UI',
+                                    padding: roomStrokeWidth,
+                                    text: "Katakomben",
+                                    wrap: "word",
+                                    verticalAlign: "middle"
+                                }
+                                let text = new Konva.Text(textConfig)
+                                // console.log(text.getSize());
+
+                                return (
+                                    <Group key={roomkey}>
+                                        {connections}
+                                        <Rect
+
+                                            x={x * roomOffset}
+                                            y={y * roomOffset}
+                                            data-coordinates={[room.xCoordinate, room.yCoordinate]}
+                                            width={roomSize}
+                                            height={roomSize}
+                                            fill={fillColor}
+                                            strokeWidth={roomStrokeWidth}
+                                            stroke={strokeColor}
+                                        />
+                                        <Text
+                                           {...textConfig}
+                                        />
+
+                                    </Group>
+                                )
+                            })}
+                            {/* {<Circle ref={currentPositionCircleRef} data-id="0,0" x={0 * roomOffset + (roomSize / 2)} y={0 * roomOffset + (roomSize / 2)} radius={roomSize / 4} fill={fillCurrentRoom} stroke={fillCurrentRoom} />} */}
+                        </Group>
+                    </Layer>
+                </Stage>}
             </div>
 
-        </>
+        </div>
     )
 }
 

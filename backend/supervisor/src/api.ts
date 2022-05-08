5a86796f677fbe2c0e00b0c0331797b9cbb5df37
raphaelsack <i20029@hb.dhbw-stuttgart.de>
2022-05-08 16:55:47 +0200

    verifylink+password reset links are generated from origin, Edited Readme

--- backend/supervisor/src/api.ts
+++ backend/supervisor/src/api.ts
@@ -30,13 +30,13 @@ export class API {
      * @param hostLink host link object
      * @param dba databaseAdapter object
      */
-    constructor(origin: string, port: number, tls: TLS, hostLink: HostLink, dba: DatabaseAdapter, salt: string, verifyLink:string , passwordResetLink:string , transporter: any, cookiehost: string) {
+    constructor(origin: string, port: number, tls: TLS, hostLink: HostLink, dba: DatabaseAdapter, salt: string, transporter: any, cookiehost: string) {
         this.origin = origin;
         this.port = port;
         this.tls = tls;
         this.hostLink = hostLink;
         this.dba = dba;
-        this.authProvider = new AuthProvider(this.dba, salt, transporter, verifyLink, passwordResetLink, cookiehost);
+        this.authProvider = new AuthProvider(this.dba, salt, transporter, origin, cookiehost);
     }
 
     /**

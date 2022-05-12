d639bb9bc7366b991a13d3a0b34c49040c94614b
raphaelsack <i20029@hb.dhbw-stuttgart.de>
2022-05-12 21:24:14 +0200

    format issues

--- frontend/src/components/Dashboard/AllDungeons/AllDungeons.tsx
+++ frontend/src/components/Dashboard/AllDungeons/AllDungeons.tsx
@@ -12,14 +12,14 @@
  */
 
 import React, { useState } from 'react';
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import AllDungeonLi from './AllDungeonsLi';
 
 import { DungeonResponseData } from '@supervisor/api';
 import { Row, Col } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
-import {supervisor} from 'src/services/supervisor'
-import {useGame} from 'src/hooks/useGame'
+import { supervisor } from 'src/services/supervisor'
+import { useGame } from 'src/hooks/useGame'
 import { SendsMessagesProps } from '../../../types/misc';
 import DungeonPasswordModal, {
     DungeonPasswordModalProps,
@@ -42,64 +42,66 @@ const AllDungeons: React.FC<AllDungeonProps & SendsMessagesProps> = ({
     const navigate = useNavigate();
     const [modalState, setModalState] = useState<DungeonPasswordModalProps>({
         show: false,
-        onConfirm: (password: string) => {},
-        onHide: () => {},
+        onConfirm: (password: string) => { },
+        onHide: () => { },
     });
 
     const onPasswordRequest = (dungeonId: string, dungeonName: string) => {
-        setModalState({ show: true, onConfirm: password => {
-            supervisor.checkPassword(
-                dungeonId,
-                password,
-                () => {
-                    setModalState({ show: false, onHide: () => { }, onConfirm: () => { } });
-                    game.setDungeon(dungeonId);
-                    game.setDungeonName(dungeonName);
-                    navigate('/select-character');
-                },
-                error => {
-                    console.log(error.error);
-                    messageCallback(error.error);
-                }
-            );
-        }, onHide: () => {
-            setModalState({ show: false, onHide: () => {}, onConfirm: () =>{}});
-            messageCallback("");
-        } });
+        setModalState({
+            show: true, onConfirm: password => {
+                supervisor.checkPassword(
+                    dungeonId,
+                    password,
+                    () => {
+                        setModalState({ show: false, onHide: () => { }, onConfirm: () => { } });
+                        game.setDungeon(dungeonId);
+                        game.setDungeonName(dungeonName);
+                        navigate('/select-character');
+                    },
+                    error => {
+                        console.log(error.error);
+                        messageCallback(error.error);
+                    }
+                );
+            }, onHide: () => {
+                setModalState({ show: false, onHide: () => { }, onConfirm: () => { } });
+                messageCallback("");
+            }
+        });
     };
 
     return (
         <div className="dashboard pt-3">
             {allDungeons.length !== 0 ? <>
-            <Row className="py-2">
-                <div className="col-3">
-                    <b><u>{t("dungeon_keys.name")}</u></b>
-                </div>
-                <div className="col-5">
-                    <b><u>{t("dungeon_keys.description")}</u></b>
-                </div>
-                <div className="col-1">
-                    <b><u>{t("dungeon_keys.players")}</u></b>
-                </div>
-                <div className="col-1 text-center">
-                    <b><u>{t("dungeon_keys.status")}</u></b>
-                </div>
-                <div className="col-2"></div>
-            </Row>
-            
-            {allDungeons.filter(dungeon => dungeon[filterKey].toLowerCase().includes(filterValue.toLowerCase())).map((dungeon, index) => {
-                return (
-                    <AllDungeonLi
-                                onPasswordRequest={onPasswordRequest}
-                                key={index}
-                                {...dungeon}
-                            />
-                )
-            })}</>
-             : 
-             <CryingLlama />
-             }
-        <DungeonPasswordModal {...modalState} />
+                <Row className="py-2">
+                    <div className="col-3">
+                        <b><u>{t("dungeon_keys.name")}</u></b>
+                    </div>
+                    <div className="col-5">
+                        <b><u>{t("dungeon_keys.description")}</u></b>
+                    </div>
+                    <div className="col-1">
+                        <b><u>{t("dungeon_keys.players")}</u></b>
+                    </div>
+                    <div className="col-1 text-center">
+                        <b><u>{t("dungeon_keys.status")}</u></b>
+                    </div>
+                    <div className="col-2"></div>
+                </Row>
+
+                {allDungeons.filter(dungeon => dungeon[filterKey].toLowerCase().includes(filterValue.toLowerCase())).map((dungeon, index) => {
+                    return (
+                        <AllDungeonLi
+                            onPasswordRequest={onPasswordRequest}
+                            key={index}
+                            {...dungeon}
+                        />
+                    )
+                })}</>
+                :
+                <CryingLlama />
+            }
+            <DungeonPasswordModal {...modalState} />
         </div>
     )
 }

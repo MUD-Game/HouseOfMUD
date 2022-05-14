dee4acdbef8ecfa3b779aa0ad4a925fdcae197ba
niklasar <niklas.arnold@web.de>
2022-05-14 15:09:47 +0200

    Dashboard style

--- frontend/src/components/Busy/CryingLlama.tsx
+++ frontend/src/components/Busy/CryingLlama.tsx
@@ -5,20 +5,16 @@ import "./index.css"
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
-const CryingLlama : React.FC = () => {
+const CryingLlama: React.FC = () => {
 
-    const {t} = useTranslation();
+    const { t } = useTranslation();
 
     return (
         <div id="sadwrapper">
-                 
-             <img src={CryingLlamaPng} alt="crying llama" className="img-fluid" />
-                 
-                 <div>
-                        <h1>{t("cryingLlama.title")}</h1>
-                <h4><Link to="/dungeon-configurator">{t("cryingLlama.text")}</Link></h4>
-                 </div>
-             </div>
+            <img src={CryingLlamaPng} alt="crying llama" className="img-fluid" />
+            <span className="crying-title text-center mt-2">{t("cryingLlama.title")}</span>
+            <span className="crying-text text-center mt-1"><Link to="/dungeon-configurator">{t("cryingLlama.text")}</Link></span>
+        </div>
     );
 }
 
